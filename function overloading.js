81ffeae5d70a3ba14b55c18abd324e30c914f403
shyamala-create <57354821+shyamala-create@users.noreply.github.com>
2021-01-27 02:06:51 +0530

    Add files via upload

---
+++ function overloading.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    return a + b;
+}
+add("Hello ", "Steve"); // returns "Hello Steve" 
+add(10, 20); // returns 30 
+function sum(name, roll_no) {
+    return name + roll_no;
+}
+sum("shyamala", 45);
+sum(34, "78");
